648939dfd97fe176b5d267299d662a8fc8627516
VirajAdiga <virajadigamaclub@gmail.com>
2023-10-23 13:22:57 +0530

    Add opening creation, application creation and project allocation email code to message mapping

--- email-service/src/main/java/com/project/emailservice/mappers/EmailCodeToMessageMapper.java
+++ email-service/src/main/java/com/project/emailservice/mappers/EmailCodeToMessageMapper.java
@@ -12,6 +12,9 @@ public class EmailCodeToMessageMapper {
     private final Map<String, String> emailCodeToMessageMap = new HashMap<>();
     public EmailCodeToMessageMapper(){
         this.emailCodeToMessageMap.put("EMAIL_CODE_001", "Hi, \nYou have created a project successfully!");
+        this.emailCodeToMessageMap.put("EMAIL_CODE_002", "Hi, \nYou have created an opening successfully!");
+        this.emailCodeToMessageMap.put("EMAIL_CODE_003", "Hi, \nYou have applied to an opening successfully!");
+        this.emailCodeToMessageMap.put("EMAIL_CODE_004", "Hi, \nYou have been allocated to a project successfully!");
     }
 
     public String getEmailBody(String emailCode){
