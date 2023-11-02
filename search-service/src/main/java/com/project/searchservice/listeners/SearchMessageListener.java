65339a1450428a42798c848b5ea64bbf146d7371
VirajAdiga <virajadigamaclub@gmail.com>
2023-11-02 11:53:23 +0530

    Update message listener to perform actions depending on type

--- search-service/src/main/java/com/project/searchservice/listeners/SearchMessageListener.java
+++ search-service/src/main/java/com/project/searchservice/listeners/SearchMessageListener.java
@@ -3,16 +3,24 @@ package com.project.searchservice.listeners;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.searchservice.dto.OpeningSearchMessage;
+import com.project.searchservice.entities.OpeningSearch;
+import com.project.searchservice.entities.enums.SearchRepoActionType;
+import com.project.searchservice.exception.InvalidActionTypeException;
 import com.project.searchservice.exception.ServerSideGeneralException;
+import com.project.searchservice.services.OpeningSearchService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageListener;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 public class SearchMessageListener implements MessageListener {
+
+    @Autowired
+    private OpeningSearchService openingSearchService;
 
     @Override
     @RabbitListener(queues = "#{queue.name}")
@@ -22,11 +30,27 @@ public class SearchMessageListener implements MessageListener {
             ObjectMapper objectMapper = new ObjectMapper();
             TypeReference<OpeningSearchMessage> mapType = new TypeReference<>() {};
             OpeningSearchMessage openingSearchMessage = objectMapper.readValue(message.getBody(), mapType);
-
-            log.info("Created search index successfully");
+            SearchRepoActionType actionType = Enum.valueOf(SearchRepoActionType.class, openingSearchMessage.getActionType());
+            OpeningSearch openingSearch = OpeningSearch.builder().
+                    id(openingSearchMessage.getId()).
+                    title(openingSearchMessage.getTitle()).
+                    details(openingSearchMessage.getDetails()).
+                    level(openingSearchMessage.getLevel()).
+                    location(openingSearchMessage.getLocation()).
+                    status(openingSearchMessage.getStatus()).
+                    projectName(openingSearchMessage.getProjectName()).
+                    skills(openingSearchMessage.getSkills()).
+                    build();
+            switch (actionType){
+                case CREATE -> openingSearchService.createOpeningForSearch(openingSearch);
+                case DELETE -> openingSearchService.deleteOpening(openingSearch);
+            }
+            log.info("Consumed message successfully");
+        }catch (IllegalArgumentException exception){
+            throw new InvalidActionTypeException("Invalid action type");
         }
         catch (Exception e) {
-            log.info("Error creating search index: " + e.getMessage());
+            log.info("Error consuming the message: " + e.getMessage());
             throw new ServerSideGeneralException("Something went wrong!");
         }
     }
