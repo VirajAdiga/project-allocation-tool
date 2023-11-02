7ae20b3e141300a2fa8ef1cde2d39a212c627639
VirajAdiga <virajadigamaclub@gmail.com>
2023-11-02 11:52:37 +0530

    Add config related to host server and elastic search

---
+++ search-service/src/main/java/com/project/searchservice/config/ServerConfig.java
@@ -0,0 +1,26 @@
+package com.project.searchservice.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import java.util.Objects;
+
+@Configuration
+public class ServerConfig {
+
+    @Autowired
+    private Environment environment;
+
+    private Integer getServerPort(){
+        return Integer.valueOf(Objects.requireNonNull(environment.getProperty("SERVER_PORT")));
+    }
+
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableWebServerFactory> webServerFactoryCustomizer() {
+        return factory -> factory.setPort(getServerPort());
+    }
+}
