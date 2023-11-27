06d5f1c4fefb0d3171a5ca05730e7fbcaca41f32
VirajAdiga <virajadigamaclub@gmail.com>
2023-11-27 17:50:42 +0530

    Use env variable for cors mapping for project allocation service

--- project-allocation-service/src/main/java/com/theja/projectallocationservice/config/CorsConfig.java
+++ project-allocation-service/src/main/java/com/theja/projectallocationservice/config/CorsConfig.java
@@ -1,7 +1,9 @@
 package com.theja.projectallocationservice.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -12,10 +14,18 @@ import java.util.List;
 // Configuration class to enable Cross-Origin Resource Sharing (CORS) for the application.
 @Configuration
 public class CorsConfig {
+
+    @Autowired
+    private Environment environment;
+
+    private String getFrontendService(){
+        return environment.getProperty("FRONTEND_SERVICE");
+    }
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Collections.singletonList(getFrontendService()));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
